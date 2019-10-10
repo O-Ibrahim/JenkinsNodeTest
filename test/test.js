19893de634d2a5e294e00b6d46255c41c924bd42
Osama Ibrahim <Osama.i.adam@gmail.com>
2019-10-10 16:10:17 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -5,14 +5,14 @@ describe("Array", function() {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
-  describe("#indexOf()", function() {
-    it("should return -1 when the value is not present", function() {
-      assert.equal([1, 2, 3].indexOf(4), 0);
-    });
-  });
-  describe("#indexOf()", function() {
-    it("should return -1 when the value is not present", function() {
-      assert.equal([1, 2, 3].indexOf(4), 1);
-    });
-  });
+  // describe("#indexOf()", function() {
+  //   it("should return -1 when the value is not present", function() {
+  //     assert.equal([1, 2, 3].indexOf(4), 0);
+  //   });
+  // });
+  // describe("#indexOf()", function() {
+  //   it("should return -1 when the value is not present", function() {
+  //     assert.equal([1, 2, 3].indexOf(4), 1);
+  //   });
+  // });
 });
